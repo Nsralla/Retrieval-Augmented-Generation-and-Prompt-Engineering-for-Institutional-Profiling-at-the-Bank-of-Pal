809ba64201c0b6d65dc6abb4ed754567622a2e66
Nsralla <nsrhassan22@gmail.com>
2025-05-16 20:46:27 +0300

    reducing pass length

--- Frontend/src/pages/signup/signup.tsx
+++ Frontend/src/pages/signup/signup.tsx
@@ -52,8 +52,8 @@ const Signup: React.FC = () => {
 
       if (!password) {
         errors.password = 'Password is required';
-      } else if (password.length < 6) {
-        errors.password = 'Password must be at least 6 characters';
+      } else if (password.length < 3) {
+        errors.password = 'Password must be at least 3 characters';
       }
 
       if (!confirmPassword) {
